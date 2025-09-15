9f4200685408e9340315bbff363c421ec60fabd8
SA-Fiha <fihaahmed@outlook.com>
2025-09-15 10:46:11 +0600

    Complete Case study

---
+++ VConnect/Models/Cases/CaseIndexVM.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace VConnect.Models.Cases
+{
+    public class CaseIndexVM
+    {
+        public Study FeaturedCase { get; set; }
+        public List<Study> AllCases { get; set; } = new();
+        public List<Category> Categories { get; set; } = new();
+        public Impact_Stats ImpactStats { get; set; } = new();
+    }
+}
